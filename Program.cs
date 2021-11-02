20a3a58a7290b84e6f2237268cabfe0b99923371
gavrun <38539655+gavrun@users.noreply.github.com>
2021-11-03 01:26:40 +0300

    prepared for single file draft

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace console_type_system
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Console");
+
+
+            //Console.ReadLine();
+            Console.ReadKey()
         }
     }
 }
+

d5a4d85b89b99f626a6a6763d6e1945ee65d20d8
gavrun <38539655+gavrun@users.noreply.github.com>
2021-11-03 01:35:36 +0300

    filled draft

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace console_type_system
 {
     class Program
@@ -13,5 +14,78 @@ namespace console_type_system
             Console.ReadKey()
         }
     }
+}
+
+
+namespace console_type_system.departments
+{
+    public interface IEmployee
+    {
+
+    }
+}
+
+
+namespace console_type_system.departments
+{
+    public class Employee
+    {
+        private int id;
+
+        private string firstName;
+        private string lastName;
+
+        private DateTime birthDay;
+
+        private string email;
+
+        private int numberOfHoursWorked;
+
+        private double wage;
+        private double? hourlyRate;
+
+        public static double taxRate = 0.15;
+
+        
+
+    }
+}
+
+
+namespace console_type_system.departments
+{
+    public class Manager : Employee
+    {
+
+    }
+}
+
+
+namespace console_type_system.departments
+{
+    public class StoreManager : Employee
+    {
+
+    }
+}
+
+
+namespace console_type_system.departments
+{
+    public class Researcher : Employee
+    {
+
+    }
+}
+
+
+namespace console_type_system.departments
+{
+    public class Developer : Employee
+    {
+
+    }
+
+    //public class JuniorDeveloper : Developer
 }
 
